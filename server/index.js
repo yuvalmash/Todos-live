2f78aacfae65e9d64f7782c1fa4129b5233b6eaf
Yuval <yuvalmaa@gmail.com>
2024-03-20 11:04:22 +0200

    revert to a dev server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, "build")))
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"))
-})
 
 const router = require('./routes');
 app.use('/api', router);
